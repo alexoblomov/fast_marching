27e38ac5b23c6ff98c70fbf37a3187f474a765fa
alexoblomov <alanna.kennard@gmail.com>
2020-02-02 14:44:10 +0100

    few edits

--- mesh_djikstra.py
+++ mesh_djikstra.py
@@ -6,17 +6,18 @@ Created on Mon Dec 16 13:43:22 2019
 @author: alex
 """
 import numpy as np
-
-# mesh and plotting imports
+import time
 import pymesh
 import matplotlib.tri as mtri
 import matplotlib.pyplot as plt
-from matplotlib import transforms
 import matplotlib.colors as mcolors
-#graph imports
 import networkx as nx
 
 from utils import create_weighted_graph, add_connectivity
+
+# NB: THE FULL VERSION OF PYMESH MUST BE INSTALLED TO RUN THIS SCRIPT
+# AS THE PIP INSTALLATION DOES NOT INCLUDE THE FUNCTION load_mesh()
+# THE FULL VERSION OF PYMESH CAN BE INSTALLED VIA git clone ..
 
 for i in np.arange(4,12,2):
     mesh_name = "poly_" + str(i) + ".off"
@@ -24,7 +25,7 @@ for i in np.arange(4,12,2):
     mesh_connectivity = "poly_connectivity_" + str(i) + ".txt"
     # load and visualize mesh
     mesh = pymesh.load_mesh(mesh_name)
-    plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
+    #plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
     
     # convert mesh to graph 
     graph = pymesh.mesh_to_graph(mesh)
@@ -32,21 +33,22 @@ for i in np.arange(4,12,2):
     weighted_graph = create_weighted_graph(graph)
     
     # add connectivity
+    start_c = time.time()
     connect = np.loadtxt(mesh_connectivity)
     weighted_graph_with_connectivity = add_connectivity(np.copy(graph[0]), weighted_graph.copy(), connect)
     
     # create networkx graph
     G = nx.from_edgelist(weighted_graph)
-    
     G_prime = nx.from_edgelist(weighted_graph_with_connectivity)
     
-    # load sources
+    # load boundary vertices and find their indices in the graph
     sources = np.loadtxt(mesh_boundary)
     src = np.where(np.isin(graph[0][:,0:2], sources[:,0:2])[:,0])[0]    
     
-    # run Dijkstra
-    length, path = nx.multi_source_dijkstra(G,set(src))
-    length_prime, path_prime = nx.multi_source_dijkstra(G_prime, set(src))
+    # run Dijkstra - to set sources on the bounday use set(src) as the sources
+    # parameter
+    length, path = nx.multi_source_dijkstra(G,{15})
+    length_prime, path_prime = nx.multi_source_dijkstra(G_prime, {15})
     
     #convert lengths into array for triplot
     dist= np.zeros(np.shape(graph[0])[0]) 
@@ -61,25 +63,28 @@ for i in np.arange(4,12,2):
     #plot
     fig1, ax1 = plt.subplots()
     ax1.set_aspect('equal')
-    for i in np.arange(len(src)):
-        tcf = ax1.plot(mesh.vertices[src[i]][0], mesh.vertices[src[i]][1] ,'r*', markersize=11)
-    tcf = plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
+    #for i in np.arange(len(src)):
+     #   tcf = ax1.plot(mesh.vertices[src[i]][0], mesh.vertices[src[i]][1] ,'r*', markersize=11)
+        
+    tcf = ax1.plot(mesh.vertices[15][0], mesh.vertices[15][1] ,'r*', markersize=11)
+    #tcf = plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
     tcf = ax1.tricontourf(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, dist)
     plt.title('Dijkstra without connectivity')
-    title = "dijkstra_poly_" + str(i) + "_srcboundary"+ ".png"
+    title = "dijkstra_poly_" + str(i) + "_gon"+ ".png"
     plt.axis('off')
     plt.savefig(title, bbox_inches = 'tight')
     
     
     fig1, ax1 = plt.subplots()
     ax1.set_aspect('equal')
-    for i in np.arange(len(src)):
-        tcf = ax1.plot(mesh.vertices[src[i]][0], mesh.vertices[src[i]][1] ,'r*', markersize=11)
-    tcf = ax1.plot(mesh.vertices[3][0], mesh.vertices[3][1] ,'r*', markersize=11)
-    tcf = plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
+    #for i in np.arange(len(src)):
+     #   tcf = ax1.plot(mesh.vertices[src[i]][0], mesh.vertices[src[i]][1] ,'r*', markersize=11)
+    tcf = ax1.plot(mesh.vertices[15][0], mesh.vertices[15][1] ,'r*', markersize=11)
+
+    #tcf = plt.triplot(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, 'ko-', lw = 0.5, alpha=0.5, ms = 0.7)
     tcf = ax1.tricontourf(mesh.vertices[:,0], mesh.vertices[:,1], mesh.faces, dist_prime)
     plt.title('Dijkstra with connectivity')
-    title = "dijkstra_connected_poly_" + str(i) + "_srcboundary"+ ".png"
+    title = "dijkstra_connected_poly_" + str(i) + "_gon"+ ".png"
     plt.axis('off')
     plt.savefig(title, bbox_inches = 'tight')
     
--- utils.py
+++ utils.py
@@ -12,14 +12,14 @@ import numpy as np
 def create_weighted_graph(graph):
     weighted_graph = []
     for edge in graph[1]:
-       weight =({'weight' : 10* distance.euclidean(graph[0][edge[0]], graph[0][edge[1]])})
+       weight =({'weight' : 10*distance.euclidean(graph[0][edge[0]], graph[0][edge[1]])})
        weighted_graph.append((edge[0],edge[1], weight))
     
     return weighted_graph
 
 def add_connectivity(nodes, weighted_graph, connect):
     nodes_to_connect = np.where(np.isin(nodes[:,0:2], connect[:,0:2])[:,0])[0]
-    weight = ({'weight' : 0.01})
+    weight = ({'weight' : 0.001})
     row = 0
     num_rows = np.shape(connect)[0]
     while row < num_rows:
